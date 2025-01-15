0ceba1e271e8eab3fe3ddb5b9f18c481b041124c
upshot65 <techmailupshot@gmail.com>
2025-01-16 00:25:51 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,13 @@ export default defineNuxtConfig({
     redirect: false,
   },
   css: ["@fortawesome/fontawesome-free/css/all.min.css"],
+  runtimeConfig: {
+    // Server-side only
+    user: process.env.EMAIL_USER, // Email is now taken from environment variables
+    pass: process.env.EMAIL_PASS, // Password is now taken from environment variables
+    // Exposed to client and server
+    public: {
+      publicApiKey: process.env.PUBLIC_API_FOR_EMAIL,
+    },
+  },
 });
