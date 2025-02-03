2168749c6b801c12a6aa8b4a0403456d6f036770
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-03 19:26:34 +0530

    Create editor.vue

---
+++ pages/admin/editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <!-- <h1 class="justify-center">Welcome to Editor</h1> -->
+
+    <ClientOnly>
+      <Quill v-model:content="body" />
+    </ClientOnly>
+    <main v-html="body"></main>
+  </section>
+</template>
+
+<script setup>
+const body = ref("");
+</script>
+
+<style></style>
