fafdb6896b1100f2bac41ba18c99d8fc5b6c36a7
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-03 20:00:00 +0530

    Update editor.vue

--- pages/admin/editor.vue
+++ pages/admin/editor.vue
@@ -7,6 +7,13 @@
     </ClientOnly>
     <main v-html="body"></main>
   </section>
+  <div class="flex items-center justify-center min-h-screen">
+    <button
+      class="w-50 py-3 mt-6 rounded-full bg-black text-white font-semibold hover:bg-gray-800 transition-colors"
+    >
+      Add Article
+    </button>
+  </div>
 </template>
 
 <script setup>
