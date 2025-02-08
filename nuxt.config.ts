f874cc581450b63907d8c74881b0b09e79e57800
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-08 13:32:23 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,50 @@
+import { fetchArticles } from "./utils/fetchArticles";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
+    "@nuxtjs/sitemap",
     "@nuxtjs/tailwindcss",
     "@nuxt/image",
     "@nuxtjs/supabase",
     "@pinia/nuxt",
   ],
+  nitro: {
+    prerender: {
+      ignore: ["/admin/category"], // Ignore this route from prerendering
+    },
+  },
+  site: {
+    url: "https://upshotbrandmedia.com", // Replace with your actual domain
+  },
+  sitemap: {
+    siteUrl: "https://www.upshotbrandmedia.com", // Replace with your actual domain
+    debug: true,
+    autoLastmod: true,
+    urls: async () => {
+      const articles = await fetchArticles();
+      return articles.map((article) => ({
+        loc: `/articles/${article.id}-${generateSlug(article.title)}`,
+        lastmod: article.updated_at,
+      }));
+    },
+  },
   supabase: {
     redirect: false,
   },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: [
+          "@vueup/vue-quill",
+          "@vueup/vue-quill/dist/vue-quill.snow.css",
+        ],
+      },
+    },
+  },
+
   css: ["@fortawesome/fontawesome-free/css/all.min.css"],
   runtimeConfig: {
     // Server-side only
@@ -22,5 +56,12 @@ export default defineNuxtConfig({
     },
   },
 });
-
-
+function generateSlug(title: string) {
+  if (!title) {
+    return "";
+  }
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-") // Replace spaces and special characters with dashes
+    .replace(/^-+|-+$/g, ""); // Remove leading and trailing dashes
+}
