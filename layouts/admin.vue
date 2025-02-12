61f626f7424209ca3235fffe61df349d65c7f42a
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-12 13:26:36 +0530

    Create admin.vue

---
+++ layouts/admin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- Header Section -->
+   Admin
+
+    <!-- Main Content Section -->
+    <main class="flex-grow bg-white-100 py-8 px-6">
+      <!-- class="max-w-7xl mx-auto" -->
+      <div>
+        <slot />
+      </div>
+    </main>
+
+    <!-- Subscribe Section -->
+
+    <!-- Footer Section -->
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* Additional styling if needed */
+</style>
