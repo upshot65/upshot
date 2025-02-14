d962292dd4732f6a5f164caefe99395a3b0acbbe
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-14 20:17:15 +0530

    Update editor.vue

--- pages/admin/editor.vue
+++ pages/admin/editor.vue
@@ -27,9 +27,9 @@
         <p v-if="headerImageUrl" class="text-sm text-green-500 mt-2">✅ Image uploaded successfully!</p>
       </div>
       
-     <!--  <ClientOnly>
+      <ClientOnly>
       <Quill v-model:content="body" />
-    //  </ClientOnly> -->
+     </ClientOnly>
 
       <div class="flex items-center gap-2 mt-4">
         <input type="checkbox" v-model="isFeatured" id="featured" class="w-4 h-4" />
@@ -98,7 +98,7 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-  
+import Quill from "~/components/Quill.vue";
 const supabase = useSupabaseClient();
 
 const title = ref("");
