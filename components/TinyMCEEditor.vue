b24a1a760bff41e2d3d1135a59f0d103644d288e
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-17 22:41:01 +0530

    Update TinyMCEEditor.vue

--- components/TinyMCEEditor.vue
+++ components/TinyMCEEditor.vue
@@ -1,29 +1,28 @@
 <script setup>
-import { ref } from "vue";
+import { defineProps, defineEmits } from "vue";
 import Editor from "@tinymce/tinymce-vue";
-
-const apiKey = "31qzu1570diuw9j5zq5rlmtzl7wgh1meb91nnqj0gfd5ddfx"; // Replace with your TinyMCE API key (or use 'no-api-key' for basic features)
-const content = ref("<p>Hello, TinyMCE in Nuxt 3! 🎉</p>");
+const apiKey = "31qzu1570diuw9j5zq5rlmtzl7wgh1meb91nnqj0gfd5ddfx";
+const props = defineProps(["modelValue"]);
+const emit = defineEmits(["update:modelValue"]);
 </script>
 
 <template>
-  <div>
-    <Editor
-      v-model="content"
-      :api-key="apiKey"
-      :init="{
-        height: 300,
-        menubar: true,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount',
-        ],
-        toolbar:
-          'undo redo | formatselect | bold italic backcolor | \
-          alignleft aligncenter alignright alignjustify | \
-          bullist numlist outdent indent | removeformat | help',
-      }"
-    />
-  </div>
+  <Editor
+    :api-key="apiKey"
+    :init="{
+      height: 300,
+      menubar: true,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount',
+      ],
+      toolbar:
+        'undo redo | formatselect | bold italic backcolor | \
+        alignleft aligncenter alignright alignjustify | \
+        bullist numlist outdent indent | removeformat | help',
+    }"
+    :model-value="modelValue"
+    @update:modelValue="emit('update:modelValue', $event)"
+  />
 </template>
