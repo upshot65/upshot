80430602ab39233e589b9b9b3b752619b734e23b
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-22 00:24:33 +0530

    Update Subscribe.vue

--- components/Subscribe.vue
+++ components/Subscribe.vue
@@ -5,17 +5,25 @@
       class="bg-blue-800 text-white max-w-6xl w-full py-10 md:py-14 lg:py-14 rounded-2xl relative flex flex-col space-y-6 items-center"
     >
       <!-- White Decorative Strips -->
-      <img class="absolute top-0 left-0 h-auto w-full max-w-[100px]" src="/images/form-shape-1.png" alt=""/>
-      <img class="absolute bottom-0 right-0 h-auto w-full max-w-[100px]" src="/images/form-shape-2.png" alt=""/>
+      <img
+        class="absolute top-0 left-0 h-auto w-full max-w-[100px]"
+        src="/images/form-shape-1.png"
+        alt=""
+      />
+      <img
+        class="absolute bottom-0 right-0 h-auto w-full max-w-[100px]"
+        src="/images/form-shape-2.png"
+        alt=""
+      />
       <div class="max-w-[739px] flex flex-col gap-4">
-      <!-- Header Text -->
+        <!-- Header Text -->
         <h2 class="text-2xl mb-4 md:text-3xl font-semibold text-center w-full">
           Subscribe to our newsletter
         </h2>
 
         <!-- Subscription Form -->
         <form
-          @submit.prevent="handleSubscribe"
+          @submit.prevent="subscribe"
           class="flex flex-col md:flex-row md:flex-wrap items-center space-y-4 md:space-y-0 w-full"
         >
           <!-- Name Input -->
@@ -52,13 +60,24 @@ import { ref } from "vue";
 
 const name = ref("");
 const email = ref("");
+const message = ref("");
 
-function handleSubscribe() {
-  // Add your subscription logic here
+const subscribe = async () => {
+  const res = await fetch("/api/subscribe", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: email.value, name: name.value }),
+  });
+  const data = await res.json();
+  console.log("--", data);
+  message.value =
+    data.status === "subscribed"
+      ? "Subscribed successfully!"
+      : "Error subscribing.";
   alert(`Subscribed with name: ${name.value} and email: ${email.value}`);
   name.value = "";
   email.value = "";
-}
+};
 </script>
 
 <style scoped></style>
