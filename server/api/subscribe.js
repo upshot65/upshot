eefd6054c01a8b220a3d129368a8b8cf70f83265
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-02-24 10:44:31 +0530

    Update subscribe.js

--- server/api/subscribe.js
+++ server/api/subscribe.js
@@ -1,8 +1,8 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const config = useRuntimeConfig();
-  const API_KEY = `b0df6e3303630b5c1a90a64c7857c470-us21`;
-  const LIST_ID = `98d48b4ee4`
+ const API_KEY = config.mailChimpKey;
+  const LIST_ID = config.audienceId;
   const DATACENTER = API_KEY.split("-")[1]; // Extract datacenter from API key
 
   const url = `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members/`;
